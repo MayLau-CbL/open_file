d25def496dc009389f20642a65f5cb5f044dd3ba
May Lau <may.lau.dev@gmail.com>
2021-03-09 14:04:33 +0800

    added close file method in ios

--- ios/Classes/OpenFilePlugin.m
+++ ios/Classes/OpenFilePlugin.m
@@ -124,6 +124,10 @@ static NSString *const CHANNEL_NAME = @"open_file";
 
             result(json);
         }
+    } else if ([@"close_file" isEqualToString:call.method]) {        
+        if(_documentController){
+            [_documentController dismissPreviewAnimated:YES];
+        }
     } else {
         result(FlutterMethodNotImplemented);
     }
